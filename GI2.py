f02daef4d88efd3fbdcd5d6ced3f9440907ce1de
Leowrin <leo.dana.30@gmail.com>
2024-07-20 11:02:45 +0200

    adding files

---
+++ GI2.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 15:48:15 2024
+
+@author: oozi
+"""
+
+# %% import
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+import request_transport as rt
+import datetime 
+import time
+import schedule
+
+
+scriptdir = os.path.dirname(__file__)
+
+
+# %% 
+def refresh_window():
+    # Redraw the windows
+    trains = rt.request_trains(rt.lausanne_request())
+    buses = rt.request_buses(rt.georgette_request())
+    create_train_grid(root, trains)
+    create_bus_grid(bus_window, buses)
+    root.update_idletasks()
+    bus_window.update_idletasks()
+
+
+# Function to create a grid for trains
+def create_train_grid(root, trains):
+    now = datetime.datetime.now().strftime('%H:%M')
+    for r, row in enumerate(trains):
+        for c, item in enumerate(row):
+            if r==0 and (c==2 or c==4) :
+                continue
+            
+            if isinstance(item, str):
+                if item.startswith("img/"):
+                    image_path = item
+                    img = Image.open(os.path.join(scriptdir, image_path))
+                    img = img.resize((118, 40))  # Resize the image to fit the cell
+                    img = ImageTk.PhotoImage(img)
+
+                    # Create a label for the image
+                    label = tk.Label(root, image=img, bg="#2d327d", borderwidth=0, relief="solid")
+                    label.image = img  # Keep a reference to the image to prevent garbage collection
+                    label.grid(row=r, column=c, sticky="nsew")
+                    
+                elif item.startswith("IC1") :
+                    image_path = "img/ic1.png"
+                    img = Image.open(os.path.join(scriptdir, image_path))
+                    img = img.resize((118, 40))  # Resize the image to fit the cell
+                    img = ImageTk.PhotoImage(img)
+
+                    # Create a label for the image
+                    label = tk.Label(root, image=img, bg="#2d327d", borderwidth=1, relief="solid")
+                    label.image = img  # Keep a reference to the image to prevent garbage collection
+                    label.grid(row=r, column=c, sticky="nsew")
+                
+                elif item.startswith("IC5") :
+                    image_path = "img/ic5.png"
+                    img = Image.open(os.path.join(scriptdir, image_path))
+                    img = img.resize((118, 40))  # Resize the image to fit the cell
+                    img = ImageTk.PhotoImage(img)
+
+                    # Create a label for the image
+                    label = tk.Label(root, image=img, bg="#2d327d", borderwidth=1, relief="solid")
+                    label.image = img  # Keep a reference to the image to prevent garbage collection
+                    label.grid(row=r, column=c, sticky="nsew")
+                
+                elif item.startswith("IR90") :
+                    image_path = "img/ir90.png"
+                    img = Image.open(os.path.join(scriptdir, image_path))
+                    img = img.resize((118, 40))  # Resize the image to fit the cell
+                    img = ImageTk.PhotoImage(img)
+
+                    # Create a label for the image
+                    label = tk.Label(root, image=img, bg="#2d327d", borderwidth=1, relief="solid")
+                    label.image = img  # Keep a reference to the image to prevent garbage collection
+                    label.grid(row=r, column=c, sticky="nsew")
+                    
+                elif item.startswith("IR15") :
+                    image_path = "img/ir15.png"
+                    img = Image.open(os.path.join(scriptdir, image_path))
+                    img = img.resize((118, 40))  # Resize the image to fit the cell
+                    img = ImageTk.PhotoImage(img)
+
+                    # Create a label for the image
+                    label = tk.Label(root, image=img, bg="#2d327d", borderwidth=1, relief="solid")
+                    label.image = img  # Keep a reference to the image to prevent garbage collection
+                    label.grid(row=r, column=c, sticky="nsew")
+                    
+                elif item.startswith("SN") :
+                    image_path = "img/sn.png"
+                    img = Image.open(os.path.join(scriptdir, image_path))
+                    img = img.resize((118, 40))  # Resize the image to fit the cell
+                    img = ImageTk.PhotoImage(img)
+
+                    # Create a label for the image
+                    label = tk.Label(root, image=img, bg="#2d327d", borderwidth=1, relief="solid")
+                    label.image = img  # Keep a reference to the image to prevent garbage collection
+                    label.grid(row=r, column=c, sticky="nsew")
+                    
+                
+                elif r == 0 and c == 1 :
+                    font = ("Helvetica", 20, "bold")
+                    fg_color = "white"
+                    label = tk.Label(root, text="Lausanne - Départs", font=font, fg=fg_color, bg="#2d327d", borderwidth=0, relief="solid")
+                    label.grid(row=r, column=c, columnspan=2, sticky="nsew")
+                    
+                elif r == 0 and c == 3 :
+                    font = ("Helvetica", 20, "bold")
+                    fg_color = "white"
+                    label = tk.Label(root, text=now, font=font, fg=fg_color, bg="#2d327d", borderwidth=0, relief="solid")
+                    label.grid(row=r, column=c, columnspan=2, sticky="nsew")
+                    
+                    
+           
+                else :
+                    if r == 0:
+                        font = ("Helvetica", 20, "bold")
+                        fg_color = "white"
+                    else:
+                        font = ("Helvetica", 16)
+                        fg_color = "white"
+
+                        if item.startswith("IR") or item.startswith("IC"):
+                            fg_color = "#EB0000"  
+                            font = ("Helvetica", 18)
+                        else:
+                            font = ("Helvetica", 16)
+                            fg_color = "white"
+
+                    if c == 4 and r != 0:
+                        fg_color = "#FFD32C"
+                    
+                    if c == 2 :
+                        label = tk.Label(root, text=" "+item, font=font, fg=fg_color, bg="#2d327d", borderwidth=1, relief="solid", anchor="w")
+                        label.grid(row=r, column=c, sticky="nsew")
+
+                    else :
+                        label = tk.Label(root, text=item, font=font, fg=fg_color, bg="#2d327d", borderwidth=1, relief="solid")
+                        label.grid(row=r, column=c, sticky="nsew")
+
+                
+
+    # Configure rows and columns to expand
+    for r in range(len(trains)):
+        root.grid_rowconfigure(r, weight=1)
+    for c in range(len(trains[0])):
+        root.grid_columnconfigure(c, weight=1)
+
+
+
+
+def create_bus_grid(window, buses):
+    now = datetime.datetime.now().strftime('%H:%M')
+    for r, row in enumerate(buses):
+        for c, item in enumerate(row):
+            
+            if isinstance(item, str):
+                font = ("Helvetica", 16)
+                fg_color = "#2596be"  # Blue color code for bus text
+
+                label = tk.Label(window, text=item, font=font, fg=fg_color, bg="white", borderwidth=1, relief="solid")
+                label.grid(row=r, column=c, sticky="nsew")
+
+    # Configure rows and columns to expand
+    for r in range(len(buses)):
+        window.grid_rowconfigure(r, weight=1)
+    for c in range(len(buses[0])):
+        window.grid_columnconfigure(c, weight=1)
+
+
+
+
+# Initialize the main window for trains
+root = tk.Tk()
+root.title("Lausanne - Departs")
+ws = root.winfo_screenwidth()  # width of the screen
+hs = root.winfo_screenheight()  # height of the screen
+
+root.geometry(str(f"{ws//3}x{hs}+0+0"))  # Set window size to one-third of the screen width and full height
+
+# Initialize the second window for buses
+bus_window = tk.Toplevel(root)
+bus_window.title("Georgette")
+bus_window.geometry(str(f"{ws//3}x{hs}+{ws//3}+0"))  # Set window size to one-third of the screen width and start from one-third
+
+# Create frames for trains and buses
+
+
+# Sample trains and buses: mix of text and image paths
+trains = rt.request_trains(rt.lausanne_request())
+buses = rt.request_buses(rt.georgette_request())
+
+# Create grids
+create_train_grid(root, trains)
+create_bus_grid(bus_window, buses)
+root.update_idletasks()
+bus_window.update_idletasks()
+
+# Run the application
+schedule.every().minute.at(":01").do(refresh_window)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ request_transport.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 15:48:15 2024
+
+@author: oozi
+"""
+
+# %% import
+import requests
+import xml.etree.ElementTree as ET
+import datetime as dt
+import numpy as np
+import tkinter as tk
+from tkinter import ttk
+
+
+
+
+  
+
+
+# %% define URLs and headers
+
+
+with open(".api_token.txt", "r") as token_file :
+    api_token = token_file.readline()
+
+url = 'https://api.opentransportdata.swiss/ojp2020'
+
+
+headers = {'Content-Type': 'application/xml',
+           'Authorization': 'Bearer ' + api_token
+           }
+
+
+# %% train station request
+
+def trainstation_request() :
+    
+    now = dt.datetime.now() + dt.timedelta(minutes=1)
+    
+    now = now.strftime('%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+    
+    with open(".didok_busstop.txt", "r") as didok_file :
+        didok = didok_file.readline()
+        
+    
+    xml_request = '''<?xml version="1.0" encoding="UTF-8"?>
+    <OJP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.siri.org.uk/siri" version="1.0" xmlns:ojp="http://www.vdv.de/ojp" xsi:schemaLocation="http://www.siri.org.uk/siri ../ojp-xsd-v1.0/OJP.xsd">
+        <OJPRequest>
+            <ServiceRequest>
+                <RequestTimestamp>'''+now+'''</RequestTimestamp>
+                <RequestorRef>API-Explorer_test</RequestorRef>
+                <ojp:OJPStopEventRequest>
+                    <RequestTimestamp>'''+now+'''</RequestTimestamp>
+                    <ojp:Location>
+                        <ojp:PlaceRef>
+                            <StopPlaceRef>'''+didok+'''</StopPlaceRef>
+                            <ojp:LocationName>
+                                <ojp:Text>-</ojp:Text>
+                            </ojp:LocationName>
+                        </ojp:PlaceRef>
+                        <ojp:DepArrTime>'''+now+'''</ojp:DepArrTime>
+                    </ojp:Location>
+                    <ojp:Params>
+                        <ojp:NumberOfResults>10</ojp:NumberOfResults>
+                        <ojp:StopEventType>departure</ojp:StopEventType>
+                        <ojp:IncludePreviousCalls>false</ojp:IncludePreviousCalls>
+                        <ojp:IncludeOnwardCalls>false</ojp:IncludeOnwardCalls>
+                        <ojp:IncludeRealtimeData>true</ojp:IncludeRealtimeData>
+                    </ojp:Params>
+                </ojp:OJPStopEventRequest>
+            </ServiceRequest>
+        </OJPRequest>
+    </OJP>'''
+    
+    return xml_request
+
+
+# %% bus stop request
+
+def busstop_request() :
+    
+    now = dt.datetime.now() + dt.timedelta(minutes=2)
+    
+    now = now.strftime('%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+
+    with open(".didok_trainstation.txt", "r") as didok_file :
+        didok = didok_file.readline()
+
+    xml_request = '''<?xml version="1.0" encoding="UTF-8"?>
+    <OJP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.siri.org.uk/siri" version="1.0" xmlns:ojp="http://www.vdv.de/ojp" xsi:schemaLocation="http://www.siri.org.uk/siri ../ojp-xsd-v1.0/OJP.xsd">
+        <OJPRequest>
+            <ServiceRequest>
+                <RequestTimestamp>'''+now+'''</RequestTimestamp>
+                <RequestorRef>API-Explorer_test</RequestorRef>
+                <ojp:OJPStopEventRequest>
+                    <RequestTimestamp>'''+now+'''</RequestTimestamp>
+                    <ojp:Location>
+                        <ojp:PlaceRef>
+                            <StopPlaceRef>'''+didok+'''</StopPlaceRef>
+                            <ojp:LocationName>
+                                <ojp:Text>-</ojp:Text>
+                            </ojp:LocationName>
+                        </ojp:PlaceRef>
+                        <ojp:DepArrTime>'''+now+'''</ojp:DepArrTime>
+                    </ojp:Location>
+                    <ojp:Params>
+                        <ojp:NumberOfResults>15</ojp:NumberOfResults>
+                        <ojp:StopEventType>departure</ojp:StopEventType>
+                        <ojp:IncludePreviousCalls>false</ojp:IncludePreviousCalls>
+                        <ojp:IncludeOnwardCalls>false</ojp:IncludeOnwardCalls>
+                        <ojp:IncludeRealtimeData>true</ojp:IncludeRealtimeData>
+                    </ojp:Params>
+                </ojp:OJPStopEventRequest>
+            </ServiceRequest>
+        </OJPRequest>
+    </OJP>'''
+
+    return xml_request
+
+
+# %% request trains
+
+def request_trains(xml_request):
+    
+    xml_string = requests.post(url, data = xml_request, headers = headers).text
+    
+    retrieved_train_data = ET.fromstring(xml_string)
+    
+    
+    trains = np.array([["img/sbb_logo.png", "Ligne", "Destination", "Voie", "Retard"]])
+
+    
+    # Define the namespaces
+    namespaces = {
+        'siri': 'http://www.siri.org.uk/siri',
+        'ojp': 'http://www.vdv.de/ojp'
+    }
+    
+    # Extract and print some information from the XML
+    stop_event_results = retrieved_train_data.findall('.//ojp:StopEventResult', namespaces)
+    
+    for result in stop_event_results:
+        
+        timetabled_time = result.find('.//ojp:ServiceDeparture/ojp:TimetabledTime', namespaces).text
+        
+        estimated_time = result.find('.//ojp:ServiceDeparture/ojp:EstimatedTime', namespaces)
+        estimated_time = estimated_time.text if estimated_time is not None else 'N/A'
+        
+        published_line_name = result.find('.//ojp:PublishedLineName/ojp:Text', namespaces).text
+        
+        destination_text = result.find('.//ojp:DestinationText/ojp:Text', namespaces).text
+        
+        destination_text = destination_text.encode('latin1').decode('utf-8')
+        
+        track = result.find('.//ojp:PlannedQuay/ojp:Text', namespaces)
+        track = track.text if track is not None else '-'
+        
+        
+        timetabled_time = dt.datetime.strptime(timetabled_time, '%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+        
+        
+        if estimated_time != 'N/A' :
+            estimated_time = dt.datetime.strptime(estimated_time, '%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+            
+            if estimated_time != timetabled_time :
+                delay = estimated_time - timetabled_time
+                delay = delay.total_seconds() / 60
+                #delay = delay.strftime("%M")
+            else:
+                delay = ""
+            
+        else:
+            delay = ""
+        
+        
+        timetabled_time = timetabled_time.strftime("%H:%M")
+        
+        
+        trains = np.concatenate((trains, [[timetabled_time, published_line_name, destination_text, track, delay]]), axis = 0)    
+        
+        
+    return trains
+
+
+# %% request buses
+
+
+def request_buses(xml_request):
+    
+    xml_string = requests.post(url, data = xml_request, headers = headers).text
+    
+    retrieved_buses_data = ET.fromstring(xml_string)
+    
+    buses = np.array([["Ligne", "Direction", "Départ"]])
+
+    
+    # Define the namespaces
+    namespaces = {
+        'siri': 'http://www.siri.org.uk/siri',
+        'ojp': 'http://www.vdv.de/ojp'
+    }
+    
+    
+    # Extract and print some information from the XML
+    stop_event_results = retrieved_buses_data.findall('.//ojp:StopEventResult', namespaces)
+    
+    
+    for result in stop_event_results:
+        
+        timetabled_time = result.find('.//ojp:ServiceDeparture/ojp:TimetabledTime', namespaces).text
+        
+        estimated_time = result.find('.//ojp:ServiceDeparture/ojp:EstimatedTime', namespaces)
+        estimated_time = estimated_time.text if estimated_time is not None else 'N/A'
+        
+        published_line_name = result.find('.//ojp:PublishedLineName/ojp:Text', namespaces).text
+        
+
+        
+        destination_text = result.find('.//ojp:DestinationText/ojp:Text', namespaces).text
+        
+        destination_text = destination_text.encode('latin1').decode('utf-8')
+        
+        track = result.find('.//ojp:PlannedQuay/ojp:Text', namespaces)
+        track = track.text if track is not None else '-'
+        
+
+        
+        if estimated_time != 'N/A' :
+            estimated_time = dt.datetime.strptime(estimated_time, '%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+            
+            awaiting_time = estimated_time - dt.datetime.now()
+            
+            awaiting_time = awaiting_time.total_seconds() // 60
+            
+        else:
+            timetabled_time = dt.datetime.strptime(timetabled_time, '%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+            
+            awaiting_time = timetabled_time - dt.datetime.now()
+            
+            awaiting_time = awaiting_time.total_seconds() // 60
+        
+        
+        awaiting_time = str(f"{int(awaiting_time)}'")
+        
+        buses = np.concatenate((buses, [[published_line_name, destination_text, awaiting_time]]), axis = 0)    
+        
+        
+    return buses
+
+
+
+# %% check functions
+if __name__ == '__main__':
+    trains = request_trains(trainstation_request())
+    buses = request_buses(busstop_request())
+
+    
+
